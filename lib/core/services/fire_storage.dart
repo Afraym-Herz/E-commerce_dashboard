413d36fc238598944107924c394387f986de1085
Afraym-Herz <afraym100herz@gmail.com>
2025-08-12 01:00:37 +0300

    create storage services and impl with fire storage

---
+++ lib/core/services/fire_storage.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:e_commerce_dashboard/core/services/storage_services.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart' as b ;
+
+
+class FireStorage implements StorageServices {
+    
+  final storageRef = FirebaseStorage.instance.ref();
+ 
+  @override
+  Future<String> uploadFile({required String path, required File file}) async {
+    String fileName = b.basename(file.path);
+    String extenstion = b.extension(fileName);
+
+    Reference imageRef = storageRef.child('$path/$fileName.$extenstion');
+    await imageRef.putFile(file);
+
+    return await imageRef.getDownloadURL(); 
+  }
+
+}
