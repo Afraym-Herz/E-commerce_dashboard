eb9f59f58c7d2bfdc7998d38e5d50b2ae47516cb
Afraym-Herz <afraym100herz@gmail.com>
2025-08-11 12:03:36 +0300

    create dashboard view and its body

--- lib/features/presentation/views/dashboard_view.dart
+++ lib/features/presentation/views/dashboard_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_dashboard/features/presentation/views/widgets/dashboard_view_body.dart';
 import 'package:flutter/material.dart';
 
 class DashboardView extends StatelessWidget {
@@ -5,6 +6,6 @@ class DashboardView extends StatelessWidget {
   static const routeName = 'dashboard';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const DashboardViewBody();
   }
 }
