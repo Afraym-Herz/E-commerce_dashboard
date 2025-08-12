a45e0d98d07d7a658cc3f50ff36061ea55eff5cb
Afraym-Herz <afraym100herz@gmail.com>
2025-08-13 00:06:25 +0300

    add product repo impl

--- lib/core/repos/product_repo/product_repo_impl.dart
+++ lib/core/repos/product_repo/product_repo_impl.dart
@@ -1,13 +1,28 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce_dashboard/core/errors/failures.dart';
 import 'package:e_commerce_dashboard/core/repos/product_repo/product_repo.dart';
+import 'package:e_commerce_dashboard/core/services/database_services.dart';
+import 'package:e_commerce_dashboard/core/utils/backend_endpoints.dart';
+import 'package:e_commerce_dashboard/features/data/models/add_product_input_model.dart';
 import 'package:e_commerce_dashboard/features/domain/entities/add_product_input_entity.dart';
 
 class ProductRepoImpl implements ProductRepo {
-  @override
-  Future<Either<Failures, void>> addProduct(AddProductInputEntity addProductInputEntity) {
-    // TODO: implement addProduct
-    throw UnimplementedError();
-  }
+  final DatabaseServices databaseServices;
 
+  ProductRepoImpl({required this.databaseServices});
+  @override
+  Future<Either<Failures, void>> addProduct(
+    AddProductInputEntity addProductInputEntity,
+  ) async {
+    try {
+      await databaseServices.addData(
+        path: BackendEndpoints.products,
+        data: AddProductInputModel.fromEntity(addProductInputEntity).toMap(),
+        documentId: addProductInputEntity.productCode,
+      );
+      return const Right(null);
+    } on Exception catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
 }
