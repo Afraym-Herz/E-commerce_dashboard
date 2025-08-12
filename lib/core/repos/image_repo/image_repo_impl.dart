5be4f3823f3fa3aef6a83f2107fad6e508dca496
Afraym-Herz <afraym100herz@gmail.com>
2025-08-12 12:10:11 +0300

    impl image repo

--- lib/core/repos/image_repo/image_repo_impl.dart
+++ lib/core/repos/image_repo/image_repo_impl.dart
@@ -1,14 +1,24 @@
 import 'dart:io';
-
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce_dashboard/core/errors/failures.dart';
 import 'package:e_commerce_dashboard/core/repos/image_repo/image_repo.dart';
+import 'package:e_commerce_dashboard/core/services/storage_services.dart';
+import 'package:e_commerce_dashboard/core/utils/backend_endpoints.dart';
 
 class ImageRepoImpl implements ImageRepo {
+  final StorageServices storageServices;
+  ImageRepoImpl({required this.storageServices});
   @override
-  Future<Either<Failures, String>> uploadImage(File image) {
-    // TODO: implement uploadImage
-    throw UnimplementedError();
+  Future<Either<Failures, String>> uploadImage(File image) async {
+    try {
+      return Right(
+        await storageServices.uploadFile(
+          path: BackendEndpoints.images,
+          file: image,
+        ),
+      );
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
   }
-
 }
