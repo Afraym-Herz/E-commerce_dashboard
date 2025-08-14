61c36c6578091101d23b76cdbb4fff11a1b23ff6
Afraym-Herz <afraym100herz@gmail.com>
2025-08-14 11:51:36 +0300

    create constants file

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+const String kUrl = 'https://ezpbrsbkjqkbclendlbt.supabase.co' ;
+const String kApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6cGJyc2JranFrYmNsZW5kbGJ0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTEwNDgwNSwiZXhwIjoyMDcwNjgwODA1fQ.oDiDiqHCK8fNzVgNBU6sybZ-I0n54SPfKBO1Qws1TVg' ;
+const String kBucket = 'fruit_images' ;
