2ad35d26ec5faf5ffe018df8606bc8c4fc7e5b34
Afraym-Herz <afraym100herz@gmail.com>
2025-09-11 00:33:22 +0300

    create orders_repo and its impl

---
+++ lib/features/orders/data/repos/orders_repo_impl.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_dashboard/core/errors/failures.dart';
+import 'package:e_commerce_dashboard/core/services/database_services.dart';
+import 'package:e_commerce_dashboard/features/orders/data/models/order_model.dart';
+import 'package:e_commerce_dashboard/features/orders/domain/entities/order_entity.dart';
+import 'package:e_commerce_dashboard/features/orders/domain/repos/orders_repo.dart';
+
+class OrdersRepoImpl implements OrdersRepo {
+  final DatabaseServices databaseServices;
+
+  OrdersRepoImpl({required this.databaseServices});
+
+  @override
+  Future<Either<Failures, OrderEntity>> getOrders({
+    required String path,
+    required String userId,
+    required String orderBy,
+  }) async {
+    try {
+      var json = await databaseServices.getDataWithOrder(
+        path: path,
+        userId: userId,
+        orderBy: orderBy,
+      );
+      var result = OrderModel.fromJson(json);
+      return Right(result.toEntity());
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
---
+++ lib/features/orders/domain/repos/orders_repo.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce_dashboard/core/errors/failures.dart';
+import 'package:e_commerce_dashboard/features/orders/domain/entities/order_entity.dart';
+
+abstract class OrdersRepo {
+
+  Future<Either<Failures, OrderEntity>> getOrders({
+    required String path,
+    required String userId,
+    required String orderBy,
+  });
+}
